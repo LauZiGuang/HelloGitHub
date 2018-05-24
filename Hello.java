6fa50499bb55c122cb66ccb350c5ee9df64016f1
lzg <LauZiGuang@163.com>
2018-05-24 17:54:50 +0800

    single print

--- Hello.java
+++ Hello.java
@@ -1,12 +1,5 @@
 public class Hello{
-	public static final String COMPANY_NAME="lzg";
 	public static void main(String args[]){
-		System.out.println("Happy brn!");
-		print();
-	}
-	public static void print(){
-		System.out.println("***************");
-		System.out.println("Hello lingling!");
-		System.out.println("***************");
-	}
+		System.out.println("************");
+		System.out.println("############");
 }
