c248e261705a48a3c1d8ba458c4fbcd6c8d27b47
lzg <LauZiGuang@163.com>
2018-05-24 17:47:17 +0800

    add print

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Happy brn!");
+		print();
+	}
+	public static void print(){
+		System.out.println("***************");
+		System.out.println("Hello lingling!");
+		System.out.println("***************");
 	}
 }
